93f8a6c7a0eb19375f12a4ba19f440398b9f4a41
Robby Grossman <robby@freerobby.com>
2011-01-16 12:34:27 -0500

    Initial commit.

---
+++ Gemfile
@@ -0,0 +1,10 @@
+source 'http://rubygems.org'
+
+gem 'heroku'
+gem 'delayed_job'
+gem 'middle_management', :git => "git://github.com/freerobby/middle_management"
+gem 'rails', '~> 3.0'
+
+group :development, :test do
+  gem 'sqlite3-ruby', :require => 'sqlite3'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HerokuMbo::Application.config.secret_token = 'f7d5a855e56fa19589ae325393974880df6b666c79f83273135744659c4ec24324369a6d4e1bd1b59e80b94a9436265528202120131fdf6cf1d3b576f8d29011'
